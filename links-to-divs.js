50c27eeef06824f267d591b7777eca7c7344004b
Eugene Novalov <evg.novalov@gmail.com>
2021-12-16 01:03:36 +0400

    Added a custom logic for the window scrolling

--- links-to-divs.js
+++ links-to-divs.js
@@ -1,14 +1,16 @@
-// const anchors = document.querySelectorAll('.header-menu-right a[href*="#"]')
-//
-// for (let anchor of anchors) {
-//   anchor.addEventListener('click', function (e) {
-//     e.preventDefault()
-//
-//     const blockID = anchor.getAttribute('href').substr(1)
-//
-//     document.getElementById(blockID).scrollIntoView({
-//       behavior: 'smooth',
-//       block: 'start'
-//     })
-//   })
-// }
+const anchors = document.querySelectorAll('.header-menu-right a[href*="#"]')
+
+for (let anchor of anchors) {
+  function onLinkClick(event) {
+    event.preventDefault()
+    const blockID = anchor.getAttribute('href').substr(1)
+    const target = document.getElementById(blockID)
+
+    window.scrollTo({
+      top: target.offsetTop - 100,
+      behavior: 'smooth'
+    })
+  }
+
+  anchor.addEventListener('click', onLinkClick)
+}
