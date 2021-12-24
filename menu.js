f4a7eb2262af5d1f6b65cae1f1c2ec60ac051ad8
Eugene Novalov <evg.novalov@gmail.com>
2021-12-25 01:24:13 +0400

    Added a smooth scrolling for a mobile menu

--- menu.js
+++ menu.js
@@ -1,5 +1,6 @@
 const handle = document.getElementById('menu-handle')
 const menu = document.getElementById('mobile-menu') // <nav>
+const anchors = document.querySelectorAll('.header-menu-right a[href*="#"], .header-mobile-menu a[href*="#"]')
 
 handle.addEventListener('click', function () {
   let opened = false
@@ -19,3 +20,26 @@ handle.addEventListener('click', function () {
     handle.classList.add('opened') // из гамбургера в крестик
   }
 })
+
+
+for (let anchor of anchors) {
+  function onLinkClick(event) {
+    event.preventDefault()
+    const blockID = anchor.getAttribute('href').substr(1)
+    const target = document.getElementById(blockID)
+
+    window.scrollTo({
+      top: target.offsetTop - 100,
+      behavior: 'smooth'
+    })
+
+    /*
+    Не сильно оптимизировано, потому что мобильная менюшка будет "закрываться"
+    даже если мы используем только десктопную менюшку
+    * */
+    menu.classList.remove('opened') // закрываем менюшку
+    handle.classList.remove('opened') // из крестика в гамбургер
+  }
+
+  anchor.addEventListener('click', onLinkClick)
+}
